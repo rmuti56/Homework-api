78d80d043fc18ac5e21c7245a3b1a5c237dadb31
chokwatcharathep <chok.watcharathep@maqe.com>
2022-03-01 22:57:33 +0700

    fix: typo

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.POST || 5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
